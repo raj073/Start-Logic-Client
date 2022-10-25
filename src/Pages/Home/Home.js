2ecead4ad7c4def6790557064e0b1c3b1d200638
raj073 <rajoni.das@outlook.com>
2022-10-26 01:48:58 +0600

    carousel added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,65 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import Image1 from '../../assets/images/Image 1.jpg';
+import Image2 from '../../assets/images/Image 2.jpg';
+import Image3 from '../../assets/images/Image 3.jpg';
+import Image4 from '../../assets/images/Image 4.jpg';
 
 const Home = () => {
     return (
-        <div>
+        <Carousel className='container'>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 rounded"
+                    src={Image1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 rounded"
+                    src={Image2}
+                    alt="Second slide"
+                />
 
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 rounded"
+                    src={Image3}
+                    alt="Third slide"
+                />
 
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 rounded"
+                    src={Image4}
+                    alt="Third slide"
+                />
 
-
-        </div>
+                <Carousel.Caption>
+                    <h3>Fourth slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 };
 
