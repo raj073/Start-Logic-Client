4c4adbecb3a80fb66802ca1e570751d46d42e4b3
raj073 <rajoni.das@outlook.com>
2022-10-27 00:06:54 +0600

    Footer Added

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import './Footer.css';
+import { Link } from 'react-router-dom';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div>
+        <div className='mt-5'>
 
+            <div className="footer-basic">
+                <footer>
+                    <div className="social">
+                        <Link >
+                            <FaFacebook className='link-icon'></FaFacebook>
+                        </Link>
+                        <Link >
+                            <FaTwitter className='link-icon'></FaTwitter>
+                        </Link>
+                        <Link >
+                            <FaInstagram className='link-icon'></FaInstagram>
+                        </Link>
+                        <Link >
+                            <FaLinkedin className='link-icon'></FaLinkedin>
+                        </Link>
 
+                    </div>
+                    <ul className="list-inline">
+                        <li className="list-inline-item">
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li className="list-inline-item">
+                            <Link to='/courses'>Courses</Link>
+                        </li>
+                        <li className="list-inline-item">
+                            <Link to='/blog'>Blog</Link>
+                        </li>
+                        <li className="list-inline-item">
+                            <Link to='/faq'>FAQ</Link>
+                        </li>
+                        <li className="list-inline-item">
+                            <Link>Privacy Policy</Link>
+                        </li>
+                    </ul>
+                    <p className="copyright">Distance Learning © 2022 @ Rajani Kanta Das</p>
+                </footer>
+            </div>
 
         </div>
     );
