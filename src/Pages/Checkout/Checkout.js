fc3a5dc516e2a34d300c300dcd0181f3e7109374
raj073 <rajoni.das@outlook.com>
2022-10-27 16:12:33 +0600

    Checkout Card updated

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
+import { FaDollarSign } from 'react-icons/fa';
+import toast from 'react-hot-toast';
 
 const Checkout = () => {
 
-    const details = useLoaderData();
+    const course = useLoaderData();
+    const { id, title, details, img_url, ratings, course_name, course_id, author, author_img_url, assessments, lession, duration, student, level, price } = course;
 
+    const handleConfirmCourse = () => {
+        toast.success('Congratulations! You Have Successfully Enrolled the Course.', {
+            position: "top-right"
+        });
+    }
 
     return (
         <div>
@@ -14,17 +22,31 @@ const Checkout = () => {
 
                 <Row className="justify-content-md-center">
 
-                    <Col lg="2">
+                    <Col lg="4">
 
                     </Col>
 
-                    <Col lg="8">
+                    <Col lg="4">
 
-                        <h1>CheckOut Private Route: {details.title}</h1>
+                        <div className="col">
+                            <div className="card h-100 rounded border-0 bg-dark">
+                                <img src={img_url} className="card-img-top" alt="Session_image" />
+                                <div className="card-body bg-dark">
+                                    <h5 className="card-title fw-bold text-white text-center">
+                                        {title}</h5>
+                                    <p class="card-text text-white-50 fs-6 text-center">
+                                        Price: <FaDollarSign></FaDollarSign> {price}
+                                    </p>
+                                </div>
+
+                                <button onClick={handleConfirmCourse} className="btn btn-block btn-primary border-0 fw-semibold rounded-0 mx-4 mb-3">
+                                    CONFIRM COURSE</button>
+                            </div>
+                        </div>
 
                     </Col>
 
-                    <Col lg="2">
+                    <Col lg="4">
 
                     </Col>
                 </Row>
